6061424bfac18ac1acd3cf85ca5891daa61f688a
Akhilesh Raj <akhileshraj@Akhileshs-Mac-mini.local>
2021-12-17 01:50:33 -0600

    finished the code by plotting the control variables, locations of actuators and the room temperatures

--- code_development/HVAC_dynamics.m
+++ code_development/HVAC_dynamics.m
@@ -1,12 +1,13 @@
 function xdot = HVAC_dynamics(t,x,A,b,c,u)
-global location count;
+global location count time_stamp control_var;
 count = count+1;
 Lyap = -diag(sum(A))+A;
 % display(location);
 location(count,:) = locator(x,location(count-1,:));
+time_stamp(count) = t;
 h = control(x,location(count,:));
+control_var(count,:) = h;
 B = diag(b);
 C = diag(c);
 xdot = (Lyap-B)*x+b*u+C*h';
-
 end
